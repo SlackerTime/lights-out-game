a02d7a1b7f6c9e1bea26f633c49c7a1eb5571bf6
diodaton@gmail.com <diodaton@gmail.com@1f0c35bc-a05b-458c-157f-d57bd6f9da0a>
2011-12-15 05:28:17 +0000

    Added new class: LightsMain.java (Runs the main program)
    Update:
    LightGame.java (Added JToolBar, JFram and JPanel, just beginnings)

--- src/local/apps/LightGame.java
+++ src/local/apps/LightGame.java
@@ -1,5 +1,59 @@
 package local.apps;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JPanel;
+import javax.swing.JToolBar;
+
 public class LightGame {
 
+	private final String title = "Lights On";
+	
+	private JFrame frame;
+	
+	private JToolBar toolBar;
+	private JButton toolFileButton;
+	private JButton toolOptionsButton;
+	private JButton toolCheatsButton;
+	
+	private JPanel mainPanel;
+	
+	public LightGame(){
+		
+		//initialize JFrame
+		frame = new JFrame();
+		frame.setTitle(title);
+		frame.setSize(640, 480);
+		frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		frame.setResizable(false);
+		
+		//initialize JToolBar
+		toolBar = new JToolBar();
+		toolBar.setSize(new Dimension(50, 50));
+		toolFileButton = new JButton("   File   ");
+		toolFileButton.setPreferredSize(new Dimension(25, 25));
+		toolOptionsButton = new JButton("   Options   ");
+		toolOptionsButton.setPreferredSize(new Dimension(25, 25));
+		toolCheatsButton = new JButton("   Cheats   ");
+		toolCheatsButton.setPreferredSize(new Dimension(25, 25));
+		
+		toolBar.add(toolFileButton);
+		toolBar.add(toolOptionsButton);
+		toolBar.add(toolCheatsButton);
+		frame.add(toolBar, BorderLayout.NORTH);
+		
+		//initialize JPanel
+		mainPanel = new JPanel();
+		mainPanel.setBackground(new Color(100, 100, 100));
+		frame.add(mainPanel);
+		
+		
+		
+		frame.setVisible(true);
+	}
 }
---
+++ src/local/apps/LightsMain.java
@@ -0,0 +1,9 @@
+package local.apps;
+
+public class LightsMain {
+
+	public static void main(String[] args){
+		LightGame game = new LightGame();
+	}
+	
+}
